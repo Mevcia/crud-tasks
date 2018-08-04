49b49a30eddf764595d52014a7112a0f96c37e42
Mevcia <ewa.a.bronowska@gmail.com>
2018-08-04 11:45:17 +0200

    Add metods saveTask and deleteTask - excercise 15.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -4,9 +4,10 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.validation.constraints.Null;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,7 +18,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTask(final Long id) {
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(final Long id) {
         return repository.findById(id);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.delete(id);
     }
 }
