ee68b18d7d8a27897d298c95003ab122fb4feecb
Mevcia <ewa.a.bronowska@gmail.com>
2018-08-20 20:53:07 +0200

    Add Optional in getTrelloBoards - completing exc. 18.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,13 +1,17 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -17,10 +21,26 @@ public class TrelloController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public void getTrelloBoards() {
+//          My code exercise 18.2
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        trelloBoards.stream().filter(n -> n.getName() != null)
-                .filter(id -> id.getId() != null)
-                .filter(n -> n.getName().contains("Kodilla"))
-                .forEach(dto -> System.out.println(dto.getId() + " " + dto.getName()));
+        String a = trelloBoards.stream().filter(trelloBoard -> trelloBoard.getName() != null && trelloBoard.getId() != null)
+                .filter(trelloBoard -> trelloBoard.getName().contains("Kodilla"))
+                .map(board -> board.getName() + " " + board.getId())
+                .collect(Collectors.joining());
+        System.out.println(a);
+
+//          Code from materials module 18.3
+//    List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//        trelloBoards.forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+//            System.out.println("This board contains lists: ");
+//            trelloBoardDto.getLists().forEach(trelloList ->
+//                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+//        });
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
     }
 }
